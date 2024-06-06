8b72312b9bd968c1f45c6c49ab65236025b73690
sirajulhassan <thayyilsiraj911@gmail.com>
2024-06-06 14:26:35 +0530

    initialCommit
    only SpringBoot sample controller

---
+++ src/main/java/com/devopsProject/devopsProject/controller/SimpleController.java
@@ -0,0 +1,12 @@
+package com.devopsProject.devopsProject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+	@GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World test!";
+    }
+}
